6ed740d1ba7a02999610569fc257fab1081fcf07
Romulo Jose de Lucena Castro Filho <romulofilhofilho@gmail.com>
2018-11-08 21:36:18 -0300

    Fix bug getting color from colorpicker

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let defaultColor = 'rgb(250, 255, 240)';
 $(function() {
   $('#submit').click(makeGrid);
   $(document).click(paintGrid);
+  color = $('.color-picker').val();
 });
 
 // Create a HTML table as a grid
@@ -36,30 +37,31 @@ function makeGrid() {
 
 //Paint upon single clicking
 function paintGrid() {
+  // Get new selected color
+  $('.color-picker').change(function() {
     color = $('.color-picker').val();
-    // Paint upon single clicking
-    $('.pixel').click(function() {
+  });
 
+  // Paint upon single clicking
+  $('.pixel').click(function() {
+    //color = $('.color-picker').val();
+    $(this).css('background-color', color);
+  });
+
+  // Paint upon clicking and dragging
+  $('.pixel').mousedown(function() {
       $(this).css('background-color', color);
-      $(this).toggleClass('painted');
+      isClicked = true; // When mouse goes down, set isClicked to true
+    })
+    .mouseup(function() {
+      isClicked = false; // When mouse goes up, set isClicked to false
     });
 
-    // Paint upon clicking and dragging
-    $('.pixel').mousedown(function() {
-        $(this).css('background-color', color);
-        isClicked = true; // When mouse goes down, set isClicked to true
-        $(this).toggleClass('painted');
-      })
-      .mouseup(function() {
-        isClicked = false; // When mouse goes up, set isClicked to false
-      });
-
-    $('.pixel').mouseover(function() {
-      if (isClicked) { // Only change css if mouse is down
-        $(this).css('background-color', color);
-        $(this).toggleClass('painted');
-      }
-    });
+  $('.pixel').mouseover(function() {
+    if (isClicked) { // Only change css if mouse is down
+      $(this).css('background-color', color);
+    }
+  });
 }
 
 // TO BE IMPLEMENTED
